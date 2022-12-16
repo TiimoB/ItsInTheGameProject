14cc54bd8eaf8cfd4121f53b76d18cec444f5c78
TiimoB <120096631+TiimoB@users.noreply.github.com>
2022-12-16 17:59:03 +0100

    Pickups (#5)
    * added pickups health, gold, shield
    
    * added pickup spawner

--- Assets/GunManager.cs
+++ Assets/GunManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
-    [SerializeField] private GunStatsSO currGun;
+   [SerializeField] private int _currGun = 0;
     [SerializeField] private GunStatsSO[] guns;
 
     [SerializeField] private GameObject bullet;
@@ -12,9 +12,28 @@ public class GunManager : MonoBehaviour
     [SerializeField] private GameObject bulletSpawn;
     private float _nextFire;
 
+    public static GunManager Instance;
+
+
+    public void IncrementGun()
+    {
+        if (_currGun + 1 >= guns.Length)
+        {
+            return;
+        }
+
+        _currGun += 1;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+            return;
+        }
+
+        Instance = this;
         _nextFire = Time.time;
     }
 
@@ -23,7 +42,7 @@ public class GunManager : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && Time.time > _nextFire)
         {
-            _nextFire = Time.time + currGun.fireRateS;
+            _nextFire = Time.time + guns[_currGun].fireRateS;
             Instantiate(bullet, bulletSpawn.transform.position, transform.rotation);
         }
     }
