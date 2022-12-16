f07d9534127163719cf361178da6ceef8f5c4ad9
TiimoB <542517@student.saxion.nl>
2022-12-16 17:58:23 +0100

    added pickup spawner

---
+++ Assets/PickupSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private GameObject[] pickups;
+    private float _nextSpawn = 0;
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > _nextSpawn)
+        {
+            SpawnPickup();
+            _nextSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    private void SpawnPickup()
+    {
+        System.Random rnd = new System.Random();
+        int pickup = rnd.Next(pickups.Length);
+        int x = rnd.Next(-width, width + 1);
+        int y = rnd.Next(-height, height + 1);
+
+        Instantiate(pickups[pickup], new Vector3(x,y,0), Quaternion.Euler(0,0,0));
+    }
+}
